7034570a9a95f015b5eba36c173217a2d1aeb9d9
39sanskar <mishrasanskar496@gmail.com>
2024-08-02 20:37:47 +0530

    Codeforces_Solution

--- B_Corner_Twist.cpp
+++ B_Corner_Twist.cpp
@@ -147,4 +147,47 @@
 // In the fourth testcase, it can be proven that it is not possible to convert grid a
 //  into grid b
 //  using the above-mentioned operations any number of times.
-
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+ 
+    int tt; cin >> tt;
+ 
+    while(tt--) {
+        int n, m; cin >> n >> m;
+        vector<string>a(n), b(n);
+ 
+        for(auto &i : a) cin >> i;
+        for(auto &i : b) cin >> i;
+ 
+        auto process = [&](int i, int j) {
+            int difference = (b[i][j] - a[i][j] + 3) % 3;
+ 
+            if(difference == 0) return;
+            else if(difference == 1) {
+                a[i][j] = (a[i][j] - '0' + 1) % 3 + '0';
+                a[i + 1][j + 1] = (a[i + 1][j + 1] - '0' + 1) % 3 + '0';
+                a[i][j + 1] = (a[i][j + 1] - '0' + 2) % 3 + '0';
+                a[i + 1][j] = (a[i + 1][j] - '0' + 2) % 3 + '0';
+            }
+            else {
+                a[i + 1][j] = (a[i + 1][j] - '0' + 1) % 3 + '0';
+                a[i][j + 1] = (a[i][j + 1] - '0' + 1) % 3 + '0';
+                a[i + 1][j + 1] = (a[i + 1][j + 1] - '0' + 2) % 3 + '0';
+                a[i][j] = (a[i][j] - '0' + 2) % 3 + '0';
+            }
+        };  
+ 
+        for(int i = 0; i < n - 1; i++) {
+            for(int j = 0; j < m - 1; j++) {
+                process(i, j);
+            }
+        }
+ 
+        cout << (a == b ? "YES\n" : "NO\n");
+    }
+}
