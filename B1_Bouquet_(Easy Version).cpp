6a2e80e286c5de7dcfa723bc597c57dbbfee0984
39sanskar <mishrasanskar496@gmail.com>
2024-08-02 16:21:47 +0530

    Codeforces_Problem

--- B1_Bouquet_(Easy Version).cpp
+++ B1_Bouquet_(Easy Version).cpp
@@ -59,4 +59,117 @@
 // . In the fourth test case, you can assemble a bouquet with (4,4,5)
 // , which gives you 13
 //  petals, and it is the maximum amount of petals that the girl can buy.
-
+#include <bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+ll mminvprime(ll a, ll b) {return expo(a, b - 2, b);}
+ll mod_mul(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a * b) % m) + m) % m;}
+ll mod_sub(ll a, ll b, ll m) {a = a % m; b = b % m; return (((a - b) % m) + m) % m;}
+ll mod_div(ll a, ll b, ll m) {a = a % m; b = b % m; return (mod_mul(a, mminvprime(b, m), m) + m) % m;}  //only for prime m
+#define fr(i,n) for (ll i=0;i<n;i++)
+#define pb push_back
+#define sz(x) x.size()
+#define ff first
+#define ss second
+#define all(v) v.begin(), v.end()
+typedef tree<long long, null_type, less_equal<long long>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define fastio() ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#ifndef ONLINE_JUDGE
+#define debug(x) cerr << #x <<" "; _print(x); cerr << endl;
+#else
+#define debug(x)
+#endif
+// #define mod (ll)(1e9+7)
+void _print(ll t) {cerr << t;}
+void _print(int t) {cerr << t;}
+void _print(string t) {cerr << t;}
+void _print(char t) {cerr << t;}
+void _print(lld t) {cerr << t;}
+void _print(double t) {cerr << t;}
+void _print(ull t) {cerr << t;}
+ 
+template <class T, class V> void _print(pair <T, V> p);
+template <class T> void _print(vector <T> v);
+template <class T> void _print(set <T> v);
+template <class T, class V> void _print(map <T, V> v);
+template <class T> void _print(multiset <T> v);
+template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
+template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
+#pragma GCC optimize("unroll-loops,O3,Ofast") //even 10^8+ also works with this
+#pragma GCC target("avx2,avx,fma,bmi,bmi2,lzcnt,popcnt")
+ 
+int knightx[8]={-1,-2,-2,-1,1,2,2,1};
+int knighty[8]={-2,-1,1,2,2,1,-1,-2};
+ 
+//...............................................................................................................................................
+ 
+// lambda dfs------------->
+// function<int(int,int)> dfs = [&](int node,int par){
+ 
+// };
+// dfs(0,-1);
+// ---------------------------------------------------------------------------------------
+// //__builtin_popcount(a) number of set bits for int
+// //__builtin_popcountll(a) number of set bits for long long
+// //<<left shift *2
+// //>>Right shift /2
+// //char a='A'|(1<<5); in uppercase 5th bit is not set and in lower case 5th bit is set
+// //char a='A'|' ';
+// //char A='a'&(~(1<<5)); unset the 5th bit
+// //char A='a'& '_';
+// //if(n&(n-1)==0) then number is power of two
+//..................................................................................................
+ 
+ 
+ 
+void solve(){
+    ll n,m;
+    cin>>n>>m;
+    vector<ll>v(n);
+    for(auto &it:v)cin>>it;
+    sort(all(v));
+    vector<ll>pre(n);
+    pre[0]=v[0];
+    for(int i=1;i<n;i++){
+        pre[i]=pre[i-1]+v[i];
+    }
+    ll ans=0;
+    for(int i=0;i<n;i++){
+        ll temp=(i==0 ? 0:pre[i-1])+m;
+        ll idx1=upper_bound(all(v),v[i]+1)-v.begin();
+        idx1--;
+        ll idx2=upper_bound(all(pre),temp)-pre.begin();
+        idx2--;
+        if(idx2<i)continue;
+        if(idx2<=idx1){
+            ans=max(ans,pre[idx2]-(i==0 ? 0:pre[i-1]));
+        }
+        else{
+            ans=max(ans,pre[idx1]-(i==0 ? 0:pre[i-1]));
+        }
+    }
+ 
+    cout<<ans<<endl;
+}
+ 
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("Error.txt","w",stderr);
+#endif
+    
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+} 
